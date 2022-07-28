6089a454f96f40666cb8716166a76dfef230f3bf
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-07-28 20:04:58 +0300

    before lightbox

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,28 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const backdrop = document.querySelector('.backdrop');
+const closeModal = document.querySelector('.close-modal')
+const listCases = document.querySelector('.cases__list');
+const modal = document.querySelector('.modal')
+// const imageToModal = document.querySelector('.img-modal');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const toggleModal = () => {
+  backdrop.classList.toggle('modal__is-hidden');
+}
 
-  function toggleModal() {
-    refs.modal.classList.toggle('modal__is-hidden');
+
+const handleClick = (event) => {
+  // // const nextImg = event.target.closest('li').nextElementSibling.querySelector('.cases__img');
+  // // const prevImg = event.target.closest('li').previousElementSibling.querySelector('.cases__img');
+  // console.log(event.target.closest('li').previousElementSibling);
+  // console.log(event.target.closest('li').nextElementSibling);
+
+  if (event.target.tagName === 'IMG') {
+    
   }
-})();
+}
+
+const handleCloseModal = (event) => {
+  toggleModal()
+}
+
+listCases.addEventListener('click', handleClick);
+closeModal.addEventListener('click', handleCloseModal)
