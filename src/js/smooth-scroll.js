871177aaacd4ceb53dad1f15c10e2162cfc6c185
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-07-29 05:06:37 +0300

    good smooth scroll

---
+++ src/js/smooth-scroll.js
@@ -0,0 +1,48 @@
+const aboutSection = document.querySelector('#about')
+const casesSection = document.querySelector('#cases')
+const blogSection = document.querySelector('#blog')
+const contactSection = document.querySelector('#contact')
+const navigate = document.querySelector('.header-nav__list');
+
+const rootElement = document.documentElement;
+
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect();
+
+  return box.top + window.pageYOffset
+}
+
+function scrollToTop(coord) {
+  const headerHeight = window.visualViewport.width < 768 ? 120 : 80
+  rootElement.scrollTo({
+    top: coord ? coord - headerHeight : 0,
+    behavior: "smooth"
+  });
+}
+
+const handleClick = (event) => {
+  const nameBtn = event.target.name;
+  
+  switch (nameBtn) {
+    case 'home':
+      scrollToTop()
+      break;
+    case 'about':
+      scrollToTop(getCoords(aboutSection));
+      break;
+    case 'cases':
+      scrollToTop(getCoords(casesSection));
+      break;
+    case 'blog':
+      scrollToTop(getCoords(blogSection));
+      break;
+    case 'contact':
+      scrollToTop(getCoords(contactSection));
+      break;
+  
+    default:
+      break;
+  }
+}
+
+navigate.addEventListener('click', handleClick)
